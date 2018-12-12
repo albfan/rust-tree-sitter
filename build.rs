7b49dc958ef40430c1312a9f3dfe8010c6531383
Alberto Fanjul <albertofanjul@gmail.com>
2018-12-12 19:21:04 +0100

    Add parsers based on C

--- build.rs
+++ build.rs
@@ -2,6 +2,7 @@ extern crate cc;
 
 use std::env;
 use std::path::PathBuf;
+use std::fs;
 
 fn main() {
     let mut config = cc::Build::new();
@@ -16,11 +17,19 @@ fn main() {
         .include(root_path.join("externals").join("utf8proc"))
         .file(root_path.join("src").join("runtime").join("runtime.c"));
 
-    if env::var("RUST_TREE_SITTER_TEST").is_ok() {
-        let parser_dir: PathBuf = ["fixtures", "tree-sitter-rust", "src"].iter().collect();
-        config
-            .file(parser_dir.join("parser.c"))
-            .file(parser_dir.join("scanner.c"));
+        let paths = fs::read_dir("fixtures").unwrap();
+
+    for path in paths {
+        let mut parser_dir: PathBuf = ["fixtures", path.unwrap().file_name().to_str().unwrap(), "src"].iter().collect();
+        if parser_dir.join("parser.c").exists() {
+           config.file(parser_dir.join("parser.c"));
+        }
+        if parser_dir.join("scanner.c").exists() {
+           config.file(parser_dir.join("scanner.c"));
+        }
+//        if parser_dir.join("scanner.cc").exists() {
+//           config.file(parser_dir.join("scanner.cc"));
+//        }
     }
 
     config.compile("tree-sitter-runtime");
